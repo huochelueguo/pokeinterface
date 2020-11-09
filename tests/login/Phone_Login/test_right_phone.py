ac4fea97732e1dfcbc5f73a30dbc95b20c08c382
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2020-11-09 09:47:02 +0800

    commit Logs

--- tests/login/Phone_Login/test_right_phone.py
+++ tests/login/Phone_Login/test_right_phone.py
@@ -8,9 +8,19 @@
 """
 # 通过pytest.ini读取测试环境---通过主路径下的conftest读取对应环境的URL---通过子目录中的conftest读取数据---本文件中读取conftest中的fixture后进行测试
 import allure
+import os
+import sys
 import pytest
 
 from commom.RequestPost import Post
+from commom.Logs import Log
+
+
+# file = os.path.dirname()
+file = 'logs'
+print(file)
+log = Log(file)
+logger = log.Logger
 
 
 class Test_Login(object):
@@ -27,6 +37,7 @@ class Test_Login(object):
         test_url = get_config[0] + right_login[1].get('path')
         test_header = right_login[1].get('headers')
         test_body = right_login[1].get('body')
+        logger.info("开始执行脚本%s:\n")
         res = Post(url=test_url, header=test_header, body=test_body).post_request()[0]
         res_code = Post(url=test_url, header=test_header, body=test_body).post_request()[1]
         test_body = right_login[1].get('body')
