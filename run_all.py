01b73aaf38f1da2a2713173d235e10afd05f340a
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2020-12-01 09:05:07 +0800

    运行入口增加日志

--- run_all.py
+++ run_all.py
@@ -7,10 +7,21 @@
 @Time:NAME.py@Time:2020/11/25 18:41
 运行主入口
 """
+import time
 import pytest
-import os
+from commom.Logs import Log
 
+if __name__ == "__main__":
+    # 调用日志模块
+    log = Log(__name__)
+    logger = log.Logger
+    # 默认运行的是当前目录及子目录的所有文件夹的测试用例
+    try:
+        print("开始执行脚本")
+        logger.info("========" + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + "========")
+        pytest.main(["-s", "-v"])
+        print("脚本执行完成")
+    except Exception as e:
+        logger.error("脚本批量执行失败！", e)
+        print("脚本批量执行失败！", e)
 
-
-# 默认运行的是当前目录及子目录的所有文件夹的测试用例
-pytest.main(["-s", "-v"])
