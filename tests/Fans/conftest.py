13cf299b9b2e7c81a6d10465835201caf87186c9
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2020-12-03 09:39:09 +0800

    粉丝数用例参数化

--- tests/Fans/conftest.py
+++ tests/Fans/conftest.py
@@ -26,6 +26,7 @@ def get_fans_token(get_config):
     # 根据主conftest中的getconfig读取环境配置信息
     if get_config[1] == 'debug':
         login_data = DEBUG_DATA[1][0][1]  # 读取数据中切割出登录所使用的数据
+        print(DEBUG_DATA)
         token = ReturnToken(url=login_data.get('path'), json=login_data.get('body'),
                             header=login_data.get('headers')).post_request()    # 使用数据登录返回token和uid
     elif get_config[1] == 'online':
@@ -53,7 +54,7 @@ def pytest_generate_tests(metafunc):
     # todo:目前取值为写死的，无法进行参数化，如果多组测试数据，token和对应组怎么插入
     if 'get_fans' in metafunc.fixturenames:
         if metafunc.config.getoption('--envi') == 'debug':
-            test_data_ids = DEBUG_DATA[0][1].split()
+            test_data_ids = DEBUG_DATA[0][1::]
             test_data = DEBUG_DATA[1][1::]
             metafunc.parametrize('get_fans', test_data, ids=test_data_ids)
         elif metafunc.config.getoption('--envi') == 'online':
