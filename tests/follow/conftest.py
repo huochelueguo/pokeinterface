9bf7b12480ee3f11ef3dd5ef5fb49de64b945659
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2020-12-08 09:04:54 +0800

    增加好友相关case

---
+++ tests/follow/conftest.py
@@ -0,0 +1,8 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:孙泽
+@Github:https://github.com/huochelueguo
+@File:conftest.py
+@Time:2020/12/8 上午9:01
+"""
---
+++ tests/follow/test_followers_zero.py
@@ -0,0 +1,9 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:孙泽
+@Github:https://github.com/huochelueguo
+@File:test_followers_zero.py
+@Time:2020/12/8 上午9:00
+@用户关注为0的情况：应当默认关注poke子
+"""
