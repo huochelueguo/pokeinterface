44a3bb2ba24b02b04fa5b9df3cca10fb56f0a0a9
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2020-12-17 09:38:59 +0800

    增加关注数case

--- tests/Fans/conftest.py
+++ tests/Fans/conftest.py
@@ -27,9 +27,9 @@ def get_fans_token(get_config):
     if get_config[1] == 'debug':
         debug_data = GetData(path=DATA_PATH, envi='debug').get_data()
         login_data = debug_data[1][0][1]  # 读取数据中切割出登录所使用的数据
-        print(debug_data)
         token = ReturnToken(url=login_data.get('path'), json=login_data.get('body'),
                             header=login_data.get('headers')).post_request()    # 使用数据登录返回token和uid
+        print(token)
     elif get_config[1] == 'online':
         online_data = GetData(path=DATA_PATH, envi='online').get_data()
         login_data = online_data[1][0][1]  # 读取数据中切割出登录所使用的数据
@@ -56,6 +56,7 @@ def get_fans_zero_token(get_config):
 
 def pytest_generate_tests(metafunc):
     # todo:目前取值为写死的，无法进行参数化，如果多组测试数据，token和对应组怎么插入
+    global test_data
     if 'get_fans' in metafunc.fixturenames:
         if metafunc.config.getoption('--envi') == 'debug':
             debug_data = GetData(path=DATA_PATH, envi='debug').get_data()
@@ -92,6 +93,14 @@ def pytest_generate_tests(metafunc):
             test_data = GetData(path=data_path, envi='online').get_data()
             metafunc.parametrize('followers_zero', test_data[1], ids=test_data[0])
             logger.info('返回参数化数据【用户0关注情况】')
+    if 'followers_not_zero' in metafunc.fixturenames:
+        data_path = os.path.join(PATH + '/test_follower_not_zero')
+        if metafunc.config.getoption('--envi') == 'debug':
+            test_data = GetData(path=data_path, envi='debug').get_data()
+        elif metafunc.config.getoption('--envi') == 'online':
+            test_data = GetData(path=data_path, envi='online').get_data()
+        metafunc.parametrize('followers_not_zero', test_data[1], ids=test_data[0])
+        logger.info('返回参数化数据【用户非0关注情况】')
 
 
 def test_one(get_fans_token):
--- tests/Fans/test_follower_not_zero.py
+++ tests/Fans/test_follower_not_zero.py
@@ -5,4 +5,32 @@
 @Github:https://github.com/huochelueguo
 @File:test_follower_not_zero.py
 @Time:NAME.py@Time:2020/12/16 14:48
+@ 用户关注非0的情况，第一个关注应该不是poke子
 """
+import sys
+import allure
+import jsonpath
+
+from commom.RequestGet import Get
+from commom.Logs import Log
+
+
+log = Log(__name__)
+logger = log.Logger
+
+
+class Test_Followings_Not_Zero(object):
+    # 注意： 本用例和test_get_fans使用同一个账号，共同使用conftest/中的get_fans_token
+    def test_followings_not_zero(self, get_config, followers_not_zero, get_fans_token):
+        test_url = get_config[0] + followers_not_zero[1].get('path')
+        test_data = list(followers_not_zero)
+        test_header = followers_not_zero[1].get('header')
+        test_header['Cookie'] = get_fans_token[0]
+        test_params = test_data[1].get('params')
+        res = Get(url=test_url, params=test_params, header=test_header).get_request()
+        res_data = res[0]
+        res_code = res[1]
+        print(res)
+        assert 1 == 1
+
+
