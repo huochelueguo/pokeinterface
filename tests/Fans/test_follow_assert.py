e558f6f44b4298b3b2387811bf31c6cf69d85772
Administrator <651809271@qq.com>
2020-12-18 21:07:04 +0800

    增加关注相关case

---
+++ tests/Fans/test_follow_assert.py
@@ -0,0 +1,10 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:孙泽
+@Github:https://github.com/huochelueguo
+@File:test_follow_assert.py
+@Time:NAME.py@Time:2020/12/18 20:53
+@ 验证我的关注，两页数据下发准确，防止翻页时下发数据错误【曾经出现过该错误】
+"""
+
