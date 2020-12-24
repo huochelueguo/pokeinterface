4c786aef2ba97a7369dceddc7e8244815cf7c8a3
Administrator <651809271@qq.com>
2020-12-24 18:47:03 +0800

    上传文件改为常量取值

--- commom/PokeConst.py
+++ commom/PokeConst.py
@@ -8,19 +8,24 @@
 """
 
 from commom import Const
-Const.MP4_NUM = '1'
-Const.M4A_NUM = '2'
-Const.JPG_NUM = '3'
-Const.TXT_NUM = '4'
+import time
 
-Const.M4A = 'm4a'
-Const.MP4 = 'mp4'
-Const.JPG = 'jpg'
-Const.TXT = 'txt'
+# upload_file类型定义
+Const.UPLOAD_FILE_TYPE_M4A_NUM = 1
+Const.UPLOAD_FILE_TYPE_MP4_NUM = 2
+Const.UPLOAD_FILE_TYPE_JPG_NUM = 3
+Const.UPLOAD_FILE_TYPE_TXT_NUM = 4
+Const.UPLOAD_FILE_TYPE_M4A = 'm4a'
+Const.UPLOAD_FILE_TYPE_MP4 = 'mp4'
+Const.UPLOAD_FILE_TYPE_JPG = 'jpg'
+Const.UPLOAD_FILE_TYPE_TXT = 'txt'
+Const.DICT = {Const.UPLOAD_FILE_TYPE_MP4_NUM: Const.UPLOAD_FILE_TYPE_MP4,
+              Const.UPLOAD_FILE_TYPE_M4A_NUM: Const.UPLOAD_FILE_TYPE_M4A,
+              Const.UPLOAD_FILE_TYPE_JPG_NUM: Const.UPLOAD_FILE_TYPE_JPG,
+              Const.UPLOAD_FILE_TYPE_TXT_NUM: Const.UPLOAD_FILE_TYPE_TXT}
 
+# 时间类型常量
+Const.TIMESTAMP_S = int(time.time())
+Const.TIMESTAMP_MS = int(round(Const.TIMESTAMP_S * 1000))
 
-Const.DICT = {Const.MP4_NUM: Const.MP4,
-              Const.M4A_NUM: Const.M4A,
-              Const.JPG_NUM: Const.JPG,
-              Const.TXT_NUM: Const.TXT}
-print(Const.DICT['1'])
+print(Const.TIMESTAMP_MS)
--- commom/Upload_File.py
+++ commom/Upload_File.py
@@ -12,6 +12,7 @@ import time
 from commom.Logs import Log
 import commom.PokeConst
 from commom import Const
+
 # 调用日志模块
 log = Log(__name__)
 logger = log.Logger
@@ -22,17 +23,12 @@ class Upload_File(object):
     封装upload方法
     """
 
-    def __init__(self, url, headers, qaz, file_path,  **kwargs):
+    def __init__(self, url, headers, qaz, file_path, **kwargs):
         """
         :param url: 请求地址
         :param headers: 请求头
         :param qaz: upload文件类型
-                 1： m4a
-                 2:  mp4
-                 3:  jpg
-                 4:  txt
-        :param data: 请求数据
-        :param files: upload文件
+        :param file_path: 文件路径
         :param kwargs: 其他requests参数，如params
         """
         self.url = url
@@ -43,32 +39,39 @@ class Upload_File(object):
     def upload(self):
         global res
         try:
-            if self.qaz == Const.JPG_NUM:
+            if self.qaz == Const.UPLOAD_FILE_TYPE_JPG_NUM:
                 res = self.__upload_image()
-            elif self.qaz == Const.MP4_NUM:
+            elif self.qaz == Const.UPLOAD_FILE_TYPE_M4A_NUM:
                 res = self.__upload_m4a()
         except Exception as result:
             print(result)
             logger.error(f'{result}')
         else:
-           logger.info(f'{Const.DICT[self.qaz]}' + " 上传成功")
-           return res
+            logger.info(f'{Const.DICT[self.qaz]}' + " 上传成功")
+            return res
 
-    # 外部只暴露upload方法，其他为私有方法
-    def __upload_image(self, **kwargs):
-        params = {'qaz': 3,
-                  'client_timestamp': int(time.time())}
+    # 获取upload文件
+    def __get_file(self):
         with open(self.file_path, 'rb') as f:
-            files = {
-                'file': ('_MG_2818.jpg', f.read(), 'multipart/form-data'),
-                'filetype': ('', 'jpg', 'multipart/form-data; charset=utf-8')}
+            return f.read()
+
+    def __upload_image(self):
+        params = {'qaz': Const.UPLOAD_FILE_TYPE_JPG_NUM,
+                  'client_timestamp': Const.TIMESTAMP_S}
+        files = {
+                'file': (str(Const.TIMESTAMP_MS)+'.jpg', self.__get_file(), 'multipart/form-data'),
+                'filetype': ('', Const.UPLOAD_FILE_TYPE_JPG, 'multipart/form-data; charset=utf-8')}
         res = requests.post(url=self.url, headers=self.header, files=files, verify=False, params=params)
         return res.json()
 
     def __upload_m4a(self):
-        params = {'qaz': 1,
-                  'client_timestamp': int(time.time())}
-        res = requests.post(url=self.url, headers=self.header, files=self.files, verify=False, params=params)
+        params = {'qaz': Const.UPLOAD_FILE_TYPE_M4A_NUM,
+                  'client_timestamp': Const.TIMESTAMP_S}
+        files = {
+                'file': (str(Const.TIMESTAMP_MS)+'.m4a', self.__get_file(), 'application/octet-stream'),
+                'filetype': ('', Const.UPLOAD_FILE_TYPE_M4A_NUM, 'multipart/form-data; charset=utf-8')
+            }
+        res = requests.post(url=self.url, headers=self.header, files=files, verify=False, params=params)
         return res.json()
 
 
@@ -77,28 +80,17 @@ if __name__ == '__main__':
     # '文件1：part:'('文件名称:file_name',  '文件值：value', '文件类型：content_type')
     url = "http://test.api.pokekara.com/api/common/upload_file"
     header = {'Cookie': 'poke_session_id'
-                    '=MTYwODYzMDg5NHxjOUd0Z1ZiVHpOX3hoNGxMZUlXSmlINEwyX2lXZmlhS0V0WVZMNUM2UXZzVTczTlBfVDlzU1hWRHdKVkFIaFlhbTdHWUJKZmUyc0k2cEtnUElqTC1tZzlzWklFR1ZzTFF8GiwvPYq-_QjikjO6kO9Op5uvJLJ-qvmQOcY0nq2eka0=;',
-          'Host': 'test.api.pokekara.com',
-          'Content-Length': '1415663'}
+                        '=MTYwODYzMDg5NHxjOUd0Z1ZiVHpOX3hoNGxMZUlXSmlINEwyX2lXZmlhS0V0WVZMNUM2UXZzVTczTlBfVDlzU1hWRHdKVkFIaFlhbTdHWUJKZmUyc0k2cEtnUElqTC1tZzlzWklFR1ZzTFF8GiwvPYq-_QjikjO6kO9Op5uvJLJ-qvmQOcY0nq2eka0=;',
+              'Host': 'test.api.pokekara.com',
+              'Content-Length': '1415663'}
 
-    # 上传image
+    # # 上传image
     # file_path = "E:/pokeinterface/datas/file/image/avater_image/_MG_2818.jpg"
-    file_path = '/Users/sunwenxiao/Desktop/_MG_2135-1.jpg'
-    # params = {'qaz': 3}
-    # with open(file_path, 'rb') as f:
-    #     files = {
-    #         'file': ('_MG_2818.jpg',  f.read(), 'multipart/form-data'),
-    #         'filetype': ('', 'jpg', 'multipart/form-data; charset=utf-8')}
-    res = Upload_File(url=url, headers=header, qaz='3', file_path=file_path).upload()
-    print(res)
+    # # file_path = '/Users/sunwenxiao/Desktop/_MG_2135-1.jpg'
+    # res = Upload_File(url=url, headers=header, qaz=3, file_path=file_path).upload()
+    # print(res)
 
     # 上传m4a
-    # music_path = 'E:/pokeinterface/datas/file/music/song_record/song_record.m4a'
-    # params_music = {'qaz': 1}
-    # with open(music_path, 'rb') as f:
-    #     files_music = {
-    #         'file': ('1608717653.m4a', f.read(), 'application/octet-stream'),
-    #         'filetype': ('', 'm4a', 'multipart/form-data; charset=utf-8')
-    #     }
-    # music_res = Upload_File(url=url, headers=header, qaz='1', files=files_music).upload()
-    # print(music_res)
+    music_path = 'E:/pokeinterface/datas/file/music/song_record/song_record.m4a'
+    music_res = Upload_File(url=url, headers=header, qaz=Const.UPLOAD_FILE_TYPE_M4A_NUM, file_path=music_path).upload()
+    print(music_res)
